6b5da5ffbd4a0b19ffaa4b5c274d5fc0f6bc70ca
Seba <seba@irr.by>
2026-09-01 12:19:13 +0300

    init nestjs prisma

---
+++ data.js
@@ -0,0 +1,31 @@
+export const data = {
+  name: 'XXX XXXXXXXXXXX',
+  position: 'Fullstack Developer',
+  email: 'xxxxxxxxx@gmail.com',
+  skills: [
+    { name: 'TypeScript' },
+    { name: 'React' },
+    { name: 'Node.js' },
+    { name: 'Next.js' },
+    { name: 'Nest.js' },
+  ],
+  experience: [
+    {
+      company: 'ООО МедиаШарм',
+      position: 'Developer',
+      startDate: '2020-01',
+      endDate: null,
+      description: 'Разработка и поддержка проектов на React, Next.js, Nest.js',
+      technologies: ['NestJS', 'PostgreSQL', 'NextJS', 'TypeScript'],
+    },
+  ],
+  projects: [
+    {
+      name: 'CRM Stork',
+      description:
+        'Система управления реализацией и учетом для дистрибьютора медицинской и косметической продукции',
+      stack: ['Next', 'Nest', 'PostgreSQL', 'Redis', 'PM2'],
+      link: 'https://github.com/sebastianofereiro',
+    },
+  ],
+};
---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ProfileModule } from './modules/profile/profile.module.js';
+import { ConfigModule } from '@nestjs/config';
+import { SkillModule } from './modules/skill/skill.module.js';
+import { ExperienceModule } from './modules/experience/experience.module.js';
+import { ProjectModule } from './modules/project/project.module.js';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    ProfileModule,
+    SkillModule,
+    ExperienceModule,
+    ProjectModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
